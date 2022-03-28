231265b99c002a0ee4cd1ca880e678af607bc5fd
Ahmed Gagan <ahmedgagan6@gmail.com>
2022-03-28 18:42:26 +0530

    First Commit

---
+++ javascripts/discourse/components/shisha-banner.js
@@ -0,0 +1,13 @@
+import { inject as service } from "@ember/service";
+import Component from "@ember/component";
+import discourseComputed from "discourse-common/utils/decorators";
+
+export default Component.extend({
+  router: service(),
+
+  @discourseComputed("router.currentRouteName")
+  shouldShow(currentRouteName) {
+    console.log(currentRouteName)
+    return currentRouteName.indexOf("discovery") > -1
+  }
+});
