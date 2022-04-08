d715a50eb0fdf5670a75f19771f5b099e7ea15f6
Ahmed Gagan <ahmedgagan6@gmail.com>
2022-04-09 00:02:02 +0530

    UI: improvements

--- javascripts/discourse/components/shisha-banner.js
+++ javascripts/discourse/components/shisha-banner.js
@@ -1,9 +1,13 @@
+import { inject as service } from "@ember/service";
 import Component from "@ember/component";
 import discourseComputed from "discourse-common/utils/decorators";
 
 export default Component.extend({
-  @discourseComputed()
-  shouldShow() {
+  router: service(),
+
+  @discourseComputed("router.currentRouteName")
+  shouldShow(currentRouteName) {
+
     return (
       window.location.pathname === "/" ||
       window.location.pathname === "/categories"
